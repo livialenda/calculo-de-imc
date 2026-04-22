38a834f7fde9274e49177f89858f873be61e7996
Lívia Trindade <168776379+livialenda@users.noreply.github.com>
2026-04-22 10:58:01 -0300

    Delete cálculo de imc directory

--- cálculo de imc/imc.py
+++
@@ -1,9 +0,0 @@
-peso = float(input("Digite o peso: "))
-altura = float(input("Digite a altura: "))
-
-IMC = peso / (altura ** 2)
-
-if IMC > 25:
-    print("Acima do peso")
-else:
-    print("Peso normal")
